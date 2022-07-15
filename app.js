25a0928c23ad59f29d4e64bec84b838abc0c3dad
gbengarock <temidayoolugbenga@yahoo.com>
2022-07-15 10:47:13 +0100

    add gitignore and procfile

--- app.js
+++ app.js
@@ -123,8 +123,11 @@ app.get('/:categoryList', function(req, res){
     })
     })
     
+let port = process.env.PORT;
+if (port == null || port == ""){
+  port = 3000;
+}
 
-
-app.listen(3000, function(){
-  console.log('server running on port 3000');
+app.listen(port, function(){
+  console.log('server started successfully');
 })
